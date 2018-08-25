ec410a0e8aeb9858cbe84b3f9d27d2c9496ad54b
Higor Barbosa <hlbarbosa@stefanini.com>
2018-08-25 18:29:25 -0300

    Projeto Testes de Integração com .NET CORE 2.1.
    Projeto Exemplo do uso do xUnit para testes de integração com .NET CORE 2.1.

---
+++ WebApp.API/WebApp.TesteIntegracao/Fixtures/TestContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System.Net.Http;
+using WebApp.API;
+
+namespace WebApp.TesteIntegracao.Fixtures
+{
+    public class TestContext
+    {
+        public HttpClient Client { get; set; }
+        private TestServer _server;
+
+        public TestContext()
+        {
+            SetupClient();
+        }
+
+        private void SetupClient()
+        {
+            _server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+
+            Client = _server.CreateClient();
+
+        }
+    }
+}
---
+++ WebApp.API/WebApp.TesteIntegracao/Scenarios/ValuesTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using FluentAssertions;
+using System.Threading.Tasks;
+using WebApp.TesteIntegracao.Fixtures;
+using Xunit;
+
+namespace WebApp.TesteIntegracao.Scenarios
+{
+    public class ValuesTest
+    {
+        private readonly TestContext _testContext;
+        public ValuesTest()
+        {
+            _testContext = new TestContext();
+        }
+
+        [Fact]
+        public async Task Values_Get_ReturnsOkResponse()
+        {
+            var response = await _testContext.Client.GetAsync("/api/values");
+            response.EnsureSuccessStatusCode();
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task Values_GetById_ValuesReturnsOkResponse()
+        {
+            var response = await _testContext.Client.GetAsync("/api/values/5");
+            response.EnsureSuccessStatusCode();
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task Values_GetById_ReturnsBadRequestResponse()
+        {
+            var response = await _testContext.Client.GetAsync("/api/values/XXX");
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+
+        [Fact]
+        public async Task Values_GetById_CorrectContentType()
+        {
+            var response = await _testContext.Client.GetAsync("/api/values/5");
+            response.EnsureSuccessStatusCode();
+            response.Content.Headers.ContentType.ToString().Should().Be("text/plain; charset=utf-8");
+        }
+    }
+}
